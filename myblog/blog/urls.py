678de4129023f7a9fc714dc6abd4ce23d9290eab
github807160646 <18693273728@163.com>
2019-04-11 17:47:38 +0800

    aaa

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('index/',views.index),
+    path('index/',views.index,name = 'index'),
     path('article/<int:article_id>/',views.article_page,name='article_page'),
-    path('edit/',views.edit_page),
+    path('edit/',views.edit_page,name = 'edit_page'),
+    path('edit/action/',views.action_page,name='edit_action'),
+    path('edit/delete/<int:article_id>/',views.edit_delete,name = 'edit_delete'),
+    path('edit/change/<int:article_id>/',views.edit_change,name = 'edit_change'),
+    path('edit/tocha/<int:article_id>/',views.to_change,name = 'to_change'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -12,6 +12,30 @@ def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'article_page.html', {'article': article})
 
+def to_change(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'change_page.html', {'article': article})
 
 def edit_page(request):
     return render(request, 'edit_page.html')
+
+def edit_delete(request,article_id):
+    models.Article.objects.filter(pk =article_id ).delete()
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+def edit_change(request, article_id):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.filter(pk=article_id).update(title = title)
+    models.Article.objects.filter(pk=article_id).update(content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+def action_page(request):
+    title = request.POST.get('title','TITLE')
+    content = request.POST.get('content','CONTENT')
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
