006f920673b3d0e03dd2deecbb0879177a6ca67c
github807160646 <18693273728@163.com>
2019-04-11 16:21:58 +0800

    aa

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,5 +3,6 @@ from . import  views
 
 urlpatterns = [
     path('index/',views.index),
-    path('article/<int:article_id>/',views.article_page,name='article_page')
+    path('article/<int:article_id>/',views.article_page,name='article_page'),
+    path('edit/',views.edit_page),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
 
+
 def index(request):
     articles = models.Article.objects.all()
-    return render(request,'index.html',{'articles':articles})
+    return render(request, 'index.html', {'articles': articles})
 
-def article_page(request,article_id):
-    article = models.Article.objects.get(pk = article_id)
-    return render(request,'article_page.html',{'article':article})
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'article_page.html', {'article': article})
+
+
+def edit_page(request):
+    return render(request, 'edit_page.html')
