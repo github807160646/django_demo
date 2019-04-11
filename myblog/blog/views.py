79452a6344475c1b4a1144b84fcec8ec35344871
github807160646 <18693273728@163.com>
2019-04-11 16:03:03 +0800

    blog

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 def index(request):
-    return HttpResponse('hello world !')
+    articles = models.Article.objects.all()
+    return render(request,'index.html',{'articles':articles})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk = article_id)
+    return render(request,'article_page.html',{'article':article})
