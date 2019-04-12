0329807517771583f632066866a7154ba6a8f8fa
github807160646 <18693273728@163.com>
2019-04-12 17:56:25 +0800

    111

--- myblog/mooc/models.py
+++ myblog/mooc/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Img(models.Model):
+    img = models.ImageField(upload_to='img')
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
